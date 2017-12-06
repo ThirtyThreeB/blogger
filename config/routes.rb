098c40eb189ba9067845a5c63d81017d80b3d521
Gabriel Aragon <thirtythreebananas@gmail.com>
2017-12-06 10:04:48 -0500

    finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Blogger::Application.routes.draw do
 	root to: 'articles#index'
 
-  resources :articles	
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :articles do
+	  resources :comments
+	end
 end
