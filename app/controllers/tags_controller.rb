f5bb6076bd6d6da19b7297d07e22df5117e39b14
Gabriel Aragon <thirtythreebananas@gmail.com>
2017-12-07 10:02:23 -0500

    added a few gems

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,7 +5,7 @@ class TagsController < ApplicationController
   end
 
 	def show
-  @tag = Tag.find(params[:id])
+    @tag = Tag.find(params[:id])
   end
 
   def destroy
@@ -15,7 +15,7 @@ class TagsController < ApplicationController
 	  flash.notice = "Tag '#{@tag.name}' Deleted!"
 
 
-	  redirect_to tags_path(@tag)
+	  redirect_to tags_path
 	end 
   	
 end
